765cfc4372ccdac75143153049d440f384a7f6f5
Kaan B Aydin <kaanbrianaydin@gmail.com>
2026-09-25 18:26:08 -0700

    Added indicators, 50 day MA, RSI, avg volume, volatility, MACD

--- src/indicators.py
+++ src/indicators.py
@@ -2,5 +2,24 @@ import pandas as pd
 
 def moving_average(data):
     data["20_day_avg"] = data["Close"].rolling(window=20).mean()
+    data["50_day_avg"] = data["Close"].rolling(window=50).mean()
 
-    
+def relative_strength_index(data, period=14):
+    theta = data["Close"].diff()
+    gain = theta.where(theta > 0, 0).rolling(window=period).mean()
+    loss = -theta.where(theta < 0, 0).rolling(window=period).mean()
+    rs = gain / loss
+    data["RSI"] = 100 - (100 / (1 + rs))
+
+def average_volume(data, period=20):
+    data["avg_volume"] = data["Volume"].rolling(window=period).mean()
+
+def volatility(data, period=20):
+    data["volatility"] = data["Close"].rolling(window=period).std()
+
+def MACD(data,short_period=12, long_period=26, signal_period=9):
+    data["EMA_Short"] = data["Close"].ewm(span=short_period, adjust=False).mean()
+    data["EMA_Long"] = data["Close"].ewm(span=long_period, adjust=False).mean()
+    data["MACD"] = data["EMA_Short"] - data["EMA_Long"]
+    data["Signal_Line"] = data["MACD"].ewm(span=signal_period, adjust=False).mean()
+    data["MACD_Histogram"] = data["MACD"] - data["Signal_Line"]
--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ from indicators import *
 
 data = get_stock_data('AAPL')
 moving_average(data)
+relative_strength_index(data)
+MACD(data)
+average_volume(data)
+volatility(data)
 
-
-print(data[["Close", "20_day_avg"]].tail(10))
+print(data[["Close", "20_day_avg", "50_day_avg", "RSI","Signal_Line","MACD_Histogram","avg_volume","volatility","EMA_Short","EMA_Long"]].tail(10))
